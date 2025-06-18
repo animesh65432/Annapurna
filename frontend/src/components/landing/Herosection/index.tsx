d24f0889ff6de16f03b0eb2d05cc899cd4ab5e31
Your Name <your.email@example.com>
2025-06-18 12:42:33 +0530

    make herocontainer

---
+++ frontend/src/components/landing/Herosection/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import styles from "./HerSection.module.scss"
+import { motion } from "framer-motion"
+
+const Herosection: React.FC = () => {
+    return (
+        <div className={styles.Container}>
+            <div className={styles.ResponsiveContainer}>
+                <div className={styles.Title}>
+                    Traditional Taste <br />
+                    Smarter Nutrition
+                </div>
+
+                <motion.div
+                    className={styles.ImgContainer}
+                    initial={{ filter: "blur(10px)", opacity: 0 }}
+                    animate={{ filter: "blur(0px)", opacity: 1 }}
+                    transition={{ duration: 1, ease: "easeOut" }}
+                >
+                    <img src='/heroicon.png' alt="Hero Icon" />
+                </motion.div>
+            </div>
+
+            <motion.div
+                className={styles.description}
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 1, delay: 0.5 }}
+            >
+                Transform everyday Indian recipes into healthier meals
+                <br /> powered by AI, rooted in Indian wisdom.
+            </motion.div>
+        </div>
+    )
+}
+
+export default Herosection
--- frontend/src/components/landing/index.tsx
+++ frontend/src/components/landing/index.tsx
@@ -1,10 +1,12 @@
 import Header from "./Header"
+import Herosection from "./Herosection"
 import styles from "./Landing.module.scss"
 
 export default function Landing() {
     return (
         <div className={styles.LandingContainer}>
             <Header />
+            <Herosection />
         </div>
     )
 }
