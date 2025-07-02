49523dfb1010af56b41d5c3631d0c260e56b735f
Your Name <your.email@example.com>
2025-07-02 11:19:06 +0530

    add postgres

--- frontend/src/config/index.ts
+++ frontend/src/config/index.ts
@@ -1,4 +1,4 @@
 export const config = {
-    API_URL: "http://localhost:8000",
+    API_URL: "https://annapurna-7exj.vercel.app",
     GOOGLE_CLIENT_ID: "37607736898-g23m11gjlelgeenj67t1d2oh5m8bv9ae.apps.googleusercontent.com"
 }
