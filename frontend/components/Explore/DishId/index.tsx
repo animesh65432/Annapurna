20290c61cbaf662fd3bdb2f4d7d11bb46417d423
Your Name <your.email@example.com>
2025-08-03 09:44:14 +0530

    fix

--- frontend/components/Explore/DishId/index.tsx
+++ frontend/components/Explore/DishId/index.tsx
@@ -5,14 +5,19 @@ import { Button } from '@/components/ui/button'
 import DishIdSkeleton from './Skeleton'
 import Image from 'next/image'
 import NotFound from './Notfound'
-import router from 'next/router'
-
+import FoodLoading from '@/components/Foodloading'
+import { useRecipeStore } from "@/store/recipe"
+import { useGenrateRecipe } from "@/hooks/useGenrateRecipe"
+import { useRouter } from 'next/router'
 type Props = {
     dishId: string
 }
 
 export default function DishId({ dishId }: Props) {
     const { dish, IsLoading, GetDish } = useGetdish();
+    const { isGenrateRecipeloading, createRecipe, setisGenrateRecipeloading } = useGenrateRecipe()
+    const { setRecipe } = useRecipeStore();
+    const router = useRouter();
 
     const fetchDish = useCallback(async (id: string) => {
         try {
@@ -42,41 +47,53 @@ export default function DishId({ dishId }: Props) {
         );
     }
 
-    const handleSeeRecipe = () => {
-        router.push({
-            pathname: '/',
-            query: { dishname: dish.name },
-        })
+    const handleSeeRecipe = async () => {
+        try {
+            const response = await createRecipe(dish.name, "", dish.diet)
+            console.log(response)
+            setRecipe(response?.recipe!)
+            router.push(`/recipe/${response?.id}`)
+        } catch (error) {
+            console.error('Error creating recipe:', error);
+        }
+        finally {
+            setisGenrateRecipeloading(false)
+        }
     };
 
     return (
         <div className="bg-[url('/dashboard/backgroundimage.png')] pb-5 flex min-h-dvh overflow-y-auto w-full flex-col gap-10 lg:gap-8">
             <Header />
 
-            <div className="relative h-[30vh] sm:h-[35vh] xl:h-[40vh] w-[59vw] sm:w-[70vw] md:w-[45vw] xl:w-[35vw] ml-auto mr-auto rounded-lg overflow-hidden shadow-lg">
-                <Image
-                    src={dish.image_url}
-                    alt={`${dish.name} - ${dish.cuisine} cuisine`}
-                    fill
-                    className="object-cover"
-                    priority
-                    sizes="(max-width: 768px) 59vw, (max-width: 1024px) 45vw, 35vw"
-                />
-            </div>
-            <div className='w-[80%] sm:w-[80%] md:w-[65%] xl:w-[75%] ml-auto mr-auto flex flex-col gap-4'>
-                <div className='flex  sm:justify-start'>
-                    <h1 className='text-black text-xl sm:text-2xl lg:text-3xl '>{dish.name}</h1>
+            {!isGenrateRecipeloading &&
+                <div className="relative h-[30vh] sm:h-[35vh] xl:h-[40vh] w-[59vw] sm:w-[70vw] md:w-[45vw] xl:w-[35vw] ml-auto mr-auto rounded-lg overflow-hidden shadow-lg">
+                    <Image
+                        src={dish.image_url}
+                        alt={`${dish.name} - ${dish.cuisine} cuisine`}
+                        fill
+                        className="object-cover"
+                        priority
+                        sizes="(max-width: 768px) 59vw, (max-width: 1024px) 45vw, 35vw"
+                    />
                 </div>
-                <div className='flex flex-col gap-2 sm:gap-0 sm:items-start'>
-                    <div className='text-[#414141] text-[1.1rem] sm:text-xl mb-1 '>About :</div>
-                    <div className='text-[#414141]  text-[0.9rem] sm:text-[1rem] sm:w-full'>
-                        {dish.description || 'No description available.'}
+            }
+            {!isGenrateRecipeloading &&
+                <div className='w-[80%] sm:w-[80%] md:w-[65%] xl:w-[75%] ml-auto mr-auto flex flex-col gap-4'>
+                    <div className='flex  sm:justify-start'>
+                        <h1 className='text-black text-xl sm:text-2xl lg:text-3xl '>{dish.name}</h1>
+                    </div>
+                    <div className='flex flex-col gap-2 sm:gap-0 sm:items-start'>
+                        <div className='text-[#414141] text-[1.1rem] sm:text-xl mb-1 '>About :</div>
+                        <div className='text-[#414141]  text-[0.9rem] sm:text-[1rem] sm:w-full'>
+                            {dish.description || 'No description available.'}
+                        </div>
+                    </div>
+                    <div className='flex w-full justify-end'>
+                        <Button onClick={handleSeeRecipe} className='bg-[#FFD059] hover:bg-[#cdb169] p-5 text-black'>See Recipe</Button>
                     </div>
                 </div>
-                <div className='flex w-full justify-end'>
-                    <Button onClick={handleSeeRecipe} className='bg-[#FFD059] hover:bg-[#cdb169] p-5 text-black'>See Recipe</Button>
-                </div>
-            </div>
+            }
+            {isGenrateRecipeloading && <FoodLoading />}
         </div>
     )
 }
--- frontend/hooks/useGenrateRecipe.ts
+++ frontend/hooks/useGenrateRecipe.ts
@@ -21,4 +21,3 @@ export const useGenrateRecipe = () => {
 
 }
 
-export default useGenrateRecipe
