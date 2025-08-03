7149ab43057ad300a28d15d1166a979821cd5b19
Your Name <your.email@example.com>
2025-08-03 09:27:51 +0530

    fix

--- frontend/components/Recipe/NutritionComparison/index.tsx
+++ frontend/components/Recipe/NutritionComparison/index.tsx
@@ -55,10 +55,10 @@ export default function NutritionComparison() {
                                 <h1 className="text-center font-[500] text-[1.4rem] md:text-2xl">
                                     Before
                                 </h1>
-                                <div className=" w-[70vw] sm:w-[60vw] md:w-[50vw] lg:w-[420px] mx-auto lg:mx-0 xl:w-[460px]  border border-[#A8A8A8] rounded-md flex flex-col justify-center  sm:justify-start sm:items-start gap-5 p-[3%] lg:p-[10%]">
-                                    <div className="flex flex-col gap-2 sw-[50vw] sm:w-[354px] ml-[6%]   sm:mx-auto">
+                                <div className=" w-[65vw] sm:w-[60vw] pt-8 pb-8 lg:pt-5 lg:pb-5  md:w-[35vw] lg:w-[320px] mx-auto lg:mx-0 xl:w-[360px]  border border-[#A8A8A8] rounded-md flex flex-col   sm:justify-start sm:items-start gap-8 lg:gap-5 p-[3%] lg:p-[4%]">
+                                    <div className="flex flex-col gap-4 sw-[50vw] sm:w-[354px] sm:mx-auto">
                                         {nutritionData?.before && Object.entries(nutritionData.before).map(([key, value]) => (
-                                            <div key={key} className="text-[#757575] flex gap-2 ">
+                                            <div key={key} className="text-[#757575] w-[70%] lg:w-[55%] mx-auto flex gap-2  ">
                                                 <div className="relative h-5 w-5 sm:h-7 sm:w-7 text-green-800">
                                                     <Image alt="dishname" fill src="/assets/Nutrition.svg" className="text-green-800" />
                                                 </div>
@@ -76,10 +76,10 @@ export default function NutritionComparison() {
                                     <Image src="/assets/dashboard/star.svg" alt="star" className="absolute  h-8 w-8 left-[60%] sm:left-[57%] lg:left-[58%] bottom-[64%] " width={200} height={200} />
                                     After
                                 </h1>
-                                <div className="bg-[#F5EFD8]   border border-[#FFD059]  w-[70vw] sm:w-[60vw] md:w-[50vw] lg:w-[420px] mx-auto lg:mx-0 xl:w-[460px]   rounded-md flex flex-col justify-center  sm:justify-start sm:items-start gap-5 p-[3%] lg:p-[10%]">
-                                    <div className="flex flex-col gap-2 sw-[50vw] sm:w-[354px] ml-[6%]   sm:mx-auto">
+                                <div className="bg-[#F5EFD8]  border border-[#FFD059]   w-[65vw] sm:w-[60vw] pt-8 pb-8 lg:pt-5 lg:pb-5  md:w-[35vw] lg:w-[320px] mx-auto lg:mx-0 xl:w-[360px]   rounded-md flex flex-col   sm:justify-start sm:items-start gap-8 lg:gap-5 p-[3%] lg:p-[4%]">
+                                    <div className="flex flex-col gap-4 sw-[50vw] sm:w-[354px] sm:mx-auto">
                                         {nutritionData?.before && Object.entries(nutritionData.after).map(([key, value]) => (
-                                            <div key={key} className="text-[#757575] flex gap-2 ">
+                                            <div key={key} className="text-[#757575] w-[70%] lg:w-[55%] mx-auto  flex gap-2 ">
                                                 <div className="relative h-5 w-5 sm:h-7 sm:w-7 text-green-800">
                                                     <Image alt="dishname" fill src="/assets/Nutrition.svg" className="text-green-800" />
                                                 </div>
