d69159a3894300c03cd0cf54bc94c2387c013064
animesh65432 <kiranduttta234@gmail.com>
2025-08-28 11:37:18 +0530

    fix

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,13 +1,12 @@
 import Dashboard from "@/components/Dashboard"
 import { useSearchParams } from 'next/navigation';
-import FoodLoading from "@/components/Foodloading";
 
 export default function Home() {
   const searchParams = useSearchParams();
   const dishname = searchParams.get('dishname');
   return (
     <>
-      <FoodLoading />
+      <Dashboard dishname={dishname} />
     </>
   );
 }
